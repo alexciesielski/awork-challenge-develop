0bdd2442e02dbc82e1c48ac59654c355d6067853
Alexander Ciesielski <alexander@ciesielski.co>
2024-09-06 15:52:52 +0300

    ci: move base-href to angular.json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,3 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -10,17 +9,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
 
-    // For development use the mock service instead of the real service
-    // since the API very quickly throws 429 Too Many Requests
-    // Using `isDevMode()` does not eliminate dead code so I just manually comment out
     // {
     //   provide: UserHttpService,
     //   useClass: UserHttpMockService,
     // },
-
-    {
-      provide: APP_BASE_HREF,
-      useValue: '/awork-challenge-develop',
-    },
   ],
 };
